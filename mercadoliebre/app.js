a9cacee1b8a163cc8662b9d1ee1ab7a84214394e
Facundo-B <fmblanco97@gmail.com>
2021-09-23 18:42:33 -0300

    añado parte de formularios

--- mercadoliebre/app.js
+++ mercadoliebre/app.js
@@ -10,3 +10,19 @@ app.listen(8080, ()=> console.log("Todo ready."));
 app.get("/", (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 })
+
+app.get("/register", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.post("/register", (req, res) =>{
+    res.send("Registrado papu")
+})
+
+app.get("/login", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
+app.post("/login", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
